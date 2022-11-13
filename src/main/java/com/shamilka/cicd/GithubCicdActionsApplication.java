bc2b81291d688e1580ddff2cfe7ca98db60a0c6e
shamilka <sham.gurusinghe@gmail.com>
2022-11-13 12:50:26 +0530

    First Commit

---
+++ src/main/java/com/shamilka/cicd/GithubCicdActionsApplication.java
@@ -0,0 +1,21 @@
+package com.shamilka.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return ("Welcome Shamilka");
+	};
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shamilka/cicd/GithubCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shamilka.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
